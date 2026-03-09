6148dde064dcb58d0301348feb03a087f93abaaf
shahadat <funkyd3v@gmail.com>
2026-03-09 12:46:16 +0600

    front: home page localization

---
+++ lang/fr/common.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'pickup_button_callout'  => 'Nous venons à vous !',
+    'pickup_button_book_now' => 'Réserver Maintenant',
+    'pickup_button_schedule' => 'Planifier un Ramassage',
+    'pickup_button_aria'     => 'Planifier un service de ramassage',
+];
