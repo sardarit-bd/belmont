9d4079fd2c0399dac71229de0ddeeb5d86160a3b
shahadat <funkyd3v@gmail.com>
2026-03-11 12:21:47 +0600

    pricing multi language

---
+++ lang/ht/pricing.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'title'       => 'Pri Senp ak Transparan',
+    'subtitle'    => 'Peye sèlman pou sa ou bezwen. Gade sèvis prensipal nou yo oswa tcheke lis pri konplè nou an.',
+    'cta'         => 'Tcheke Pri Konplè',
+    'note'        => '*Pri konplè disponib pou tout kategori sèvis.',
+    'sneak_title'       => 'Nettoye Sèk',
+    'sneak_description' => 'Ou vle konnen pri nou yo? Men yon ti gade nan atik netwayaj sèk ki pi popilè nou yo. Nou itilize solvan ekolojik ki fò sou tach men dou pou twal ou yo.',
+    'item_suit'   => 'Kostim (2 moso)',
+    'item_dress'  => 'Rad',
+    'item_pants'  => 'Pantalon',
+    'item_blouse' => 'Blòz',
+];
