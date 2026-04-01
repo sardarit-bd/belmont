28940dca04e546f8be2c8726f1663488fc907178
funkyd3v <funkyd3v@gmail.com>
2026-04-01 16:03:59 +0600

    Add missing project files to the repository

--- lang/en/testimonial.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'title'    => 'What Our Customers Say',
-    'subtitle' => 'Join thousands of satisfied customers who trust Belmont Dry Cleaners for their garment care needs.',
-];
--- lang/fr/testimonial.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'title'    => 'Ce Que Disent Nos Clients',
-    'subtitle' => 'Rejoignez des milliers de clients satisfaits qui font confiance à Belmont Dry Cleaners pour l’entretien de leurs vêtements.',
-];
